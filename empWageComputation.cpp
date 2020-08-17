68d650cefdfa36c22f9d9e445eff9f0a8ee84356
vegirevathi <vegirevathi@gmail.com>
2020-08-17 23:22:58 +0530

    ADDED: Part time working hours and computed its wage

--- empWageComputation.cpp
+++ empWageComputation.cpp
@@ -7,13 +7,16 @@ int main() {
 	cout << "Welcome to Employee wage computation" << endl;
    srand(time(NULL));
 
-	int empRatePerHour = 20, empWage, empHrs;
-   int empStatus;
-   empStatus = rand() % 2;
+	int empRatePerHour = 20, partTime = 1, fullTime = 2;
+   int empStatus, empWage, empHrs;
+   empStatus = rand() % 3;
 
-   if (empStatus == 1) {
-      cout << "Employee is present" <<endl;
+   if (empStatus == fullTime) {
+      cout << "Employee is full time present" <<endl;
 		empHrs = 8;
+	} else if (empStatus == partTime) {
+		cout << "Employee is part time present" <<endl;
+		empHrs = 4;
    } else {
       cout << "Employee is absent" <<endl;
 		empHrs = 0;
