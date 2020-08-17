06122a910c83a9983a1f6dbb15b3d21853697a4c
vegirevathi <vegirevathi@gmail.com>
2020-08-17 23:16:45 +0530

    ADDED: Employee rate per hour to calculate employee wage

--- empWageComputation.cpp
+++ empWageComputation.cpp
@@ -7,13 +7,18 @@ int main() {
 	cout << "Welcome to Employee wage computation" << endl;
    srand(time(NULL));
 
+	int empRatePerHour = 20, empWage, empHrs;
    int empStatus;
    empStatus = rand() % 2;
 
    if (empStatus == 1) {
       cout << "Employee is present" <<endl;
+		empHrs = 8;
    } else {
       cout << "Employee is absent" <<endl;
+		empHrs = 0;
    }
+	empWage = empHrs * empRatePerHour;
+	cout << "Employee wage is " <<empWage <<endl;
    return 0;
 }
