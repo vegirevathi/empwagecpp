73ba950edff05f5f39ff5fb29786924b0729357c
vegirevathi <vegirevathi@gmail.com>
2020-08-20 22:31:02 +0530

    ADDED: Multiple employess and saved its data in text file

--- empWageComputation.cpp
+++ empWageComputation.cpp
@@ -2,11 +2,13 @@
 #include <cstdlib>
 #include <ctime>
 #include <fstream>
+#include <vector>
+#include <unistd.h>
 using namespace std;
 
-void write(int);
+vector <int> employeeID;
 
-int main() {
+int computeEmpWage() {
 	cout << "Welcome to Employee wage computation" << endl;
    srand(time(NULL));
 
@@ -20,33 +22,58 @@ int main() {
 while (totalEmpHrs <= maxHrsInMonth && totalWorkingDays <= numOfWorkingDays) {
 	totalWorkingDays++;
    empStatus = rand() % 3;
+
 	switch (empStatus) {
    case 1:
       cout <<"DAY" <<totalWorkingDays <<" : Employee is full time present" <<endl;
 		empHrs = 8;
 		break;
+
 	case 2:
 		cout <<"DAY" <<totalWorkingDays <<" : Employee is part time present" <<endl;
 		empHrs = 4;
 		break;
+
    default:
       cout <<"DAY" <<totalWorkingDays <<" : Employee is absent"<<endl;
 		empHrs = 0;
 		break;
-   }
+	   }
+
 	totalEmpHrs += empHrs;
 	}
-	totalEmpWage = totalEmpHrs * empRatePerHour;
-	cout << "Total employee wage is " <<totalEmpWage <<endl;
 
-	write(totalEmpWage);
-   return 0;
+	totalEmpWage = totalEmpHrs * empRatePerHour;
+	cout << "Total employee wage is " << totalEmpWage <<endl;
+
+	return totalEmpWage;
 }
 
-void write(int totalEmpWage) {
-	ofstream myfile ("wages.txt");
+void write(string fileName) {
+	fstream myfile;
+	myfile.open(fileName, ios::out | ios::trunc);
+
 	if (myfile.is_open()) {
-		myfile <<"Total employee wage for the month is " <<totalEmpWage;
-		myfile.close();
+		myfile << "Employee ID, Monthly Wage\n";
+
+		for (int i = 0; i < employeeID.size(); i++)
+		{
+			myfile << (i + 1) << ", " << (employeeID[i]) <<endl;
+		}
 	}
+	myfile.close();
+}
+
+int main() {
+	int totalEmployees;
+
+	cout << "Enter no of employees : " <<endl;
+	cin >> totalEmployees;
+
+	for (int i = 0; i < totalEmployees; i++)
+	{
+		int empWage = computeEmpWage();
+		employeeID.push_back(empWage);
+	}
+	write("wages.txt");
 }
