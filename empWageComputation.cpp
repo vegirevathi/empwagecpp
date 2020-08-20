ce637b9821611ea0fbcf9e248ac13dfae4e5b7f7
vegirevathi <vegirevathi@gmail.com>
2020-08-21 00:10:40 +0530

    ADDED: Multiple months data

--- empWageComputation.cpp
+++ empWageComputation.cpp
@@ -6,10 +6,9 @@
 #include <unistd.h>
 using namespace std;
 
-vector <int> employeeID;
+void write(string, string, string, int);
 
-int computeEmpWage() {
-	cout << "Welcome to Employee wage computation" << endl;
+int computeEmpWage(string employeeID, string month) {
    srand(time(NULL));
 
 	int empRatePerHour = 20;
@@ -17,7 +16,7 @@ int computeEmpWage() {
 	int maxHrsInMonth = 100;
 	int partTime = 1;
 	int fullTime = 2;
-   int empStatus, empWage, empHrs, totalEmpWage = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+   int empStatus, empWage, empHrs=0, totalEmpWage=0, totalEmpHrs = 0, totalWorkingDays = 0;
 
 while (totalEmpHrs <= maxHrsInMonth && totalWorkingDays <= numOfWorkingDays) {
 	totalWorkingDays++;
@@ -25,17 +24,17 @@ while (totalEmpHrs <= maxHrsInMonth && totalWorkingDays <= numOfWorkingDays) {
 
 	switch (empStatus) {
    case 1:
-      cout <<"DAY" <<totalWorkingDays <<" : Employee is full time present" <<endl;
+		//cout <<"DAY" <<totalWorkingDays <<" : Employee is full time present" <<endl;
 		empHrs = 8;
 		break;
 
 	case 2:
-		cout <<"DAY" <<totalWorkingDays <<" : Employee is part time present" <<endl;
+		//cout <<"DAY" <<totalWorkingDays <<" : Employee is part time present" <<endl;
 		empHrs = 4;
 		break;
 
    default:
-      cout <<"DAY" <<totalWorkingDays <<" : Employee is absent"<<endl;
+      //cout <<"DAY" <<totalWorkingDays <<" : Employee is absent"<<endl;
 		empHrs = 0;
 		break;
 	   }
@@ -44,36 +43,37 @@ while (totalEmpHrs <= maxHrsInMonth && totalWorkingDays <= numOfWorkingDays) {
 	}
 
 	totalEmpWage = totalEmpHrs * empRatePerHour;
-	cout << "Total employee wage is " << totalEmpWage <<endl;
+	cout << "Employee ID : " << employeeID << "Month : " << month <<" Salary : " << totalEmpWage <<endl;
 
 	return totalEmpWage;
+	write("wages.txt", employeeID, month, totalEmpWage);
 }
 
-void write(string fileName) {
+void write(string filename, string employeeID, string month, int totalEmpWage) {
 	fstream myfile;
-	myfile.open(fileName, ios::out | ios::trunc);
+	myfile.open(filename, ios::out | ios::trunc);
 
 	if (myfile.is_open()) {
-		myfile << "Employee ID, Monthly Wage\n";
-
-		for (int i = 0; i < employeeID.size(); i++)
-		{
-			myfile << (i + 1) << ", " << (employeeID[i]) <<endl;
-		}
+		myfile << employeeID << "-----" << month << "-----" << totalEmpWage << endl;
 	}
+
 	myfile.close();
 }
 
 int main() {
-	int totalEmployees;
+	cout << "Welcome to Employee wage computation" << endl;
 
-	cout << "Enter no of employees : " <<endl;
-	cin >> totalEmployees;
+	string month[3] = { "Jan", "Feb", "Mar"};
+	string employeeNames[4] = { "001", "002", "003", "004" };
 
-	for (int i = 0; i < totalEmployees; i++)
+	for (int i = 0; i < 4; i++)
 	{
-		int empWage = computeEmpWage();
-		employeeID.push_back(empWage);
+		for (int j = 0; j < 3 ; j++)
+		{
+			string monthName = month[j];
+			string employeeID = employeeNames[i];
+
+			computeEmpWage(employeeID, monthName);
+		}
 	}
-	write("wages.txt");
 }
